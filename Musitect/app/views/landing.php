29512e52bd6b808c17b2bfcb55a391bca3b3aa4d
Jasper Boyd <jasperboyd@lavabit.com>
2013-09-11 23:09:34 -0400

    Init Project
    Set up the laravel project

---
+++ Musitect/app/views/landing.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Musitect: The Songwriter's ToolKit</title>
+</head>
+<body>
+    <header>
+        <h1>Musitect</h1> 
+        <h2> A Songwriter's Toolkit</h2> 
+    </header> 
+    <article> 
+    	<h3> What Do You Get? </h3> 
+    	<ul> 
+    		<li>Lyrics</li>
+    		<li>Chords</li>
+    	</ul>
+    </article>
+    <section> 
+        <h3>Sign In</h3> 
+        <!--FORM GOES HERE--> 
+    </section>
+    <section>
+        <h3>Register</h3>
+        <!--FORM GOES HERE--> 
+    </section>
+</body>
+</html>
