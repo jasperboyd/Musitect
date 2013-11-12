453560c63dc30a6c169a613e4581543513479d51
Jasper Boyd <jasperboyd@lavabit.com>
2013-11-12 13:52:29 -0500

    Installed Ardent for Validation
    Added ardent lines to the User Model

--- musitect/app/models/User.php
+++ musitect/app/models/User.php
@@ -2,10 +2,12 @@
 
 use Eloquent;
 
+use LaravelBook\Ardent\Ardent;
+
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Ardent implements UserInterface, RemindableInterface {
 
 	/**
 	 * The database table used by the model.
