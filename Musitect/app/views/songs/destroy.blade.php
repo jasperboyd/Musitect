959d43f7a1e07e8b28c08c3ca05c4b0669aa0ba0
Jasper Boyd <jasperboyd@lavabit.com>
2013-12-10 15:33:20 -0500

    Added exit action to songs.destroy
    Just in case they change their mind.

--- Musitect/app/views/songs/destroy.blade.php
+++ Musitect/app/views/songs/destroy.blade.php
@@ -7,6 +7,7 @@
 {{Form::open(array('method' => 'DELETE', 'route' => ['song.destroy', $song->id] ))}}
 
 	<p>Are you sure you want to delete {{{ $song->title }}}? {{Form::submit('Absolutely!')}}</p> 
+	<p>{{link_to_action('HomeController@index', "No I've changed my mind")}}</p>
 
 {{Form::close()}}
 
